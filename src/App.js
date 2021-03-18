4d59e44119a6059dbcfacaa1813b17e81923c9d8
Keven Turvi <kevenmiguel999@gmail.com>
2021-03-18 11:21:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { TableComponent } from './TableComponent'
+
+
+
+export const App = () => {
+    return (
+        <div className = 'container bg-ligth'>
+            <h1>Tabla de usuarios</h1>
+            <hr/>
+            <TableComponent />
+        </div>
+    )
+}
